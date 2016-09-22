1046b6c70a542bf7bdab88a6bf60c30992e812a9
Daniel Šimko <dsimko@redhat.com>
2016-09-22 17:54:39 +0200

    Improvement of Aggregation and Admin Pages

--- saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AdminPage.java
+++ saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AdminPage.java
@@ -1,15 +1,23 @@
 
 package org.jboss.qa.tool.saatr.web.page;
 
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.jboss.qa.tool.saatr.domain.build.BuildDocument;
 import org.jboss.qa.tool.saatr.domain.config.ConfigDocument;
-import org.jboss.qa.tool.saatr.repo.build.BuildRepository;
-import org.jboss.qa.tool.saatr.repo.config.ConfigRepository;
+import org.jboss.qa.tool.saatr.domain.config.QueryDocument;
 import org.springframework.data.mongodb.core.MongoOperations;
 
 /**
@@ -19,60 +27,52 @@ import org.springframework.data.mongodb.core.MongoOperations;
 public class AdminPage extends BasePage<Void> {
 
     @Inject
-    private BuildRepository buildRepository;
-
-    @Inject
-    private ConfigRepository configRepository;
-
-    @Inject
     private MongoOperations mongoOperations;
 
     @SuppressWarnings("unused")
     private String results;
 
     public AdminPage() {
-        add(new Link<Void>("deleteAllBuilds") {
+        add(new RefreshingView<String>("collections") {
 
             @Override
-            public void onClick() {
-                buildRepository.deleteAll();
-                results = "All Builds deleted.";
+            protected Iterator<IModel<String>> getItemModels() {
+                List<IModel<String>> models = Arrays.asList(Model.of(BuildDocument.COLLECTION_NAME), Model.of(ConfigDocument.COLLECTION_NAME), Model.of(QueryDocument.COLLECTION_NAME));
+                return models.iterator();
             }
-        });
-        add(new Link<Void>("showBuildsIndexes") {
 
             @Override
-            public void onClick() {
-                showAllIndexes(BuildDocument.COLLECTION_NAME);
-            }
-        });
-        add(new Link<Void>("showBuildsStats") {
+            protected void populateItem(Item<String> item) {
+                item.add(new Label("name", item.getModelObject()));
+                item.add(new Link<Void>("deleteAll") {
 
-            @Override
-            public void onClick() {
-                showStats(BuildDocument.COLLECTION_NAME);
-            }
-        });
-        add(new Link<Void>("deleteAllConfigs") {
+                    @Override
+                    public void onClick() {
+                        mongoOperations.getCollection(item.getModelObject()).drop();;
+                        results = MessageFormat.format("All {0} deleted.", item.getModelObject());
+                    }
+                });
+                item.add(new Link<Void>("showIndexes") {
 
-            @Override
-            public void onClick() {
-                configRepository.deleteAll();
-                results = "All Configs deleted.";
-            }
-        });
-        add(new Link<Void>("showConfigsIndexes") {
+                    @Override
+                    public void onClick() {
+                        showAllIndexes(item.getModelObject());
+                    }
+                });
+                item.add(new Link<Void>("showStats") {
 
-            @Override
-            public void onClick() {
-                showAllIndexes(ConfigDocument.COLLECTION_NAME);
-            }
-        });
-        add(new Link<Void>("showConfigsStats") {
+                    @Override
+                    public void onClick() {
+                        showStats(item.getModelObject());
+                    }
+                });
+                item.add(new Link<Void>("showAllRows") {
 
-            @Override
-            public void onClick() {
-                showStats(ConfigDocument.COLLECTION_NAME);
+                    @Override
+                    public void onClick() {
+                        showAllRows(item.getModelObject());
+                    }
+                });
             }
         });
         add(new Label("results", new PropertyModel<>(this, "results")));
@@ -91,7 +91,22 @@ public class AdminPage extends BasePage<Void> {
     }
 
     private void showStats(String collectionName) {
-        results = mongoOperations.getCollection(collectionName).getStats().toJson();
+        try {
+            results = mongoOperations.getCollection(collectionName).getStats().toJson();
+        } catch (Exception e) {
+            results = e.getMessage();
+        }
+    }
+
+    private void showAllRows(String collectionName) {
+        StringBuilder builder = new StringBuilder();
+        mongoOperations.getCollection(collectionName).find().forEach(r -> {
+            if (builder.length() != 0) {
+                builder.append("\n");
+            }
+            builder.append(r);
+        });
+        results = builder.toString();
     }
 
 }
--- saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AggregationPage.java
+++ saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AggregationPage.java
@@ -19,7 +19,6 @@ import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.model.AbstractReadOnlyModel;
-import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
@@ -43,60 +42,68 @@ public class AggregationPage extends BasePage<Void> {
     @Inject
     private QueryRepository queryRepository;
 
-    private final QueryDocument query = new QueryDocument();
+    private QueryDocument query = new QueryDocument();
 
     public AggregationPage() {
-        IModel<String> pipelinesModel = new Model<>();
-        IModel<QueryDocument> predefinedPipelinesModel = new Model<>();
-        IModel<String> collectionModel = new Model<>();
         IModel<String> resultModel = new Model<>();
-        Form<Void> form = new Form<Void>("form") {
-
-            @Override
-            protected void onSubmit() {
-                try {
-                    resultModel.setObject(buildRepository.aggregate(pipelinesModel.getObject()));
-                } catch (Exception e) {
-                    error(e.getMessage());
-                    log.info(e.getMessage(), e);
-                }
-            }
-        };
+        Form<Void> form = new Form<Void>("form");
         add(form.setOutputMarkupId(true));
         form.add(new BootstrapFeedbackPanel("feedback", new ContainerFeedbackMessageFilter(form)));
-        form.add(new DropDownChoice<String>("collection", collectionModel, queryRepository.findDistinctCategories()) {
-        }.add(new OnChangeAjaxBehavior() {
+        form.add(new DropDownChoice<String>("category", new PropertyModel<>(this, "query.category"), queryRepository.findDistinctCategories()).add(
+                new OnChangeAjaxBehavior() {
 
-            @Override
-            protected void onUpdate(AjaxRequestTarget target) {
-                query.setCategory(collectionModel.getObject());
-                target.add(form);
-            }
-        }));
-        form.add(new DropDownChoice<QueryDocument>("predefinedPipelines", predefinedPipelinesModel, new AbstractReadOnlyModel<List<QueryDocument>>() {
+                    @Override
+                    protected void onUpdate(AjaxRequestTarget target) {
+                        target.add(form);
+                    }
+                }));
+        form.add(new DropDownChoice<QueryDocument>("name", new PropertyModel<>(this, "query"), new AbstractReadOnlyModel<List<QueryDocument>>() {
 
             @Override
             public List<QueryDocument> getObject() {
-                if (collectionModel.getObject() != null) {
-                    return queryRepository.findByCategory(collectionModel.getObject());
+                if (query != null && query.getCategory() != null) {
+                    return queryRepository.findByCategory(query.getCategory());
                 }
                 return Collections.emptyList();
             }
-        }, new ChoiceRenderer<>("name")) {
-        }.setNullValid(true).add(new OnChangeAjaxBehavior() {
+        }, new ChoiceRenderer<>("name")).setNullValid(true).add(new OnChangeAjaxBehavior() {
 
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
-                pipelinesModel.setObject(predefinedPipelinesModel.getObject().getQuery());
-                query.setName(predefinedPipelinesModel.getObject().getName());
-                query.setQuery(predefinedPipelinesModel.getObject().getQuery());
                 target.add(form);
             }
         }));
-        form.add(new TextArea<>("pipelines", pipelinesModel));
+        form.add(new TextArea<>("query", new PropertyModel<>(this, "query.query")));
+        form.add(new AjaxButton("search") {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                try {
+                    resultModel.setObject(buildRepository.aggregate(query.getQuery()));
+                } catch (Exception e) {
+                    error(e);
+                    log.debug(e.getMessage(), e);
+                }
+            }
+        });
+        form.add(new AjaxButton("delete") {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                try {
+                    queryRepository.delete(query);
+                    query = new QueryDocument();
+                } catch (Exception e) {
+                    log.debug(e.getMessage(), e);
+                    form.error(e);
+                }
+                target.add(form);
+            }
+
+        });
         add(new Label("result", resultModel));
         final ModalWindow modalWindow = new ModalWindow("modal");
-        modalWindow.setContent(new WindowContent(ModalWindow.CONTENT_ID, new PropertyModel<>(this, "query")));
+        modalWindow.setContent(new WindowContent(ModalWindow.CONTENT_ID));
         modalWindow.setCssClassName(ModalWindow.CSS_CLASS_GRAY);
         modalWindow.setInitialHeight(450);
         modalWindow.setTitle("Save Query as..");
@@ -112,19 +119,20 @@ public class AggregationPage extends BasePage<Void> {
 
     private class WindowContent extends Fragment {
 
-        public WindowContent(String id, IModel<QueryDocument> model) {
-            super(id, "frag1", AggregationPage.this, new CompoundPropertyModel<>(model));
+        public WindowContent(String id) {
+            super(id, "frag1", AggregationPage.this);
             Form<Void> form = new Form<Void>("form");
             add(form.setOutputMarkupId(true));
             form.add(new BootstrapFeedbackPanel("feedback", new ContainerFeedbackMessageFilter(form)));
-            form.add(new TextField<>("category").setRequired(true));
-            form.add(new TextField<>("name").setRequired(true));
-            form.add(new TextArea<>("query").setRequired(true));
+            form.add(new TextField<>("category", new PropertyModel<>(AggregationPage.this, "query.category")).setRequired(true));
+            form.add(new TextField<>("name", new PropertyModel<>(AggregationPage.this, "query.name")).setRequired(true));
+            form.add(new TextArea<>("query", new PropertyModel<>(AggregationPage.this, "query.query")).setRequired(true));
             form.add(new AjaxButton("save") {
 
                 @Override
                 protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                    queryRepository.save(model.getObject());
+                    query.setId(null);
+                    query = queryRepository.save(query);
                     info("Successfully saved.");
                     target.add(form);
                 }
