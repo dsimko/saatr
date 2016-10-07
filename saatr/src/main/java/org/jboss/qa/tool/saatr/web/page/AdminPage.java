724a50cef4db0e16f395e36b7f5252d090e56937
Daniel Šimko <dsimko@redhat.com>
2016-10-07 14:45:22 +0200

    removed temporary link

--- saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AdminPage.java
+++ saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/AdminPage.java
@@ -1,8 +1,6 @@
 
 package org.jboss.qa.tool.saatr.web.page;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -19,23 +17,15 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.jboss.qa.tool.saatr.domain.build.BuildDocument;
 import org.jboss.qa.tool.saatr.domain.build.ConsoleTextDocument;
-import org.jboss.qa.tool.saatr.domain.build.TestcaseDocument;
-import org.jboss.qa.tool.saatr.domain.build.TestsuiteDocument;
 import org.jboss.qa.tool.saatr.domain.config.ConfigDocument;
 import org.jboss.qa.tool.saatr.domain.config.QueryDocument;
-import org.jboss.qa.tool.saatr.repo.build.BuildRepository;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 
 /**
  * @author dsimko@redhat.com
  */
 @SuppressWarnings("serial")
 public class AdminPage extends BasePage<Void> {
-
-    @Inject
-    private BuildRepository buildRepository;
 
     @Inject
     private MongoOperations mongoOperations;
@@ -95,45 +85,6 @@ public class AdminPage extends BasePage<Void> {
                 getSession().invalidateNow();
             }
         });
-        add(new Link<Void>("tmp") {
-
-            @Override
-            public void onClick() {
-                buildRepository.findAll().forEach(b -> {
-                    int failedTestsuites = 0, errorTestsuites = 0, testcases = 0, failedTestcases = 0, errorTestcases = 0, skippedTestcases = 0;
-                    for (TestsuiteDocument testsuite : b.getTestsuites()) {
-                        if (testsuite.getStatus() == TestsuiteDocument.Status.Error) {
-                            errorTestsuites++;
-                        }
-                        if (testsuite.getStatus() == TestsuiteDocument.Status.Failure) {
-                            failedTestsuites++;
-                        }
-                        for (TestcaseDocument testcase : testsuite.getTestcases()) {
-                            testcases++;
-                            if (testcase.getStatus() == TestcaseDocument.Status.Error) {
-                                errorTestcases++;
-                            }
-                            if (testcase.getStatus() == TestcaseDocument.Status.Failure) {
-                                failedTestcases++;
-                            }
-                            if (testcase.getStatus() == TestcaseDocument.Status.Skipped) {
-                                skippedTestcases++;
-                            }
-                        }
-                    }
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("failedTestsuites", failedTestsuites),
-                            BuildDocument.class);
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("errorTestsuites", errorTestsuites), BuildDocument.class);
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("testcases", testcases), BuildDocument.class);
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("failedTestcases", failedTestcases), BuildDocument.class);
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("errorTestcases", errorTestcases), BuildDocument.class);
-                    mongoOperations.updateFirst(Query.query(where("id").is(b.getId())), Update.update("skippedTestcases", skippedTestcases),
-                            BuildDocument.class);
-
-                });
-            }
-        });
-
     }
 
     private void showAllIndexes(String collectionName) {
