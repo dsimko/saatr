14724fe5487af0d7ccc8d824d16244be5aa22a6b
Daniel Šimko <dsimko@redhat.com>
2016-10-13 19:21:41 +0200

    Added AJAX timer for keeping http session alive

--- saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/BasePage.java
+++ saatr/src/main/java/org/jboss/qa/tool/saatr/web/page/BasePage.java
@@ -2,10 +2,15 @@
 package org.jboss.qa.tool.saatr.web.page;
 
 import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.GenericWebPage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.time.Duration;
 import org.jboss.qa.tool.saatr.web.comp.bootstrap.BootstrapNavbarLink;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Base page for all web pages.
@@ -15,7 +20,10 @@ import org.jboss.qa.tool.saatr.web.comp.bootstrap.BootstrapNavbarLink;
  * @param <T>
  */
 @SuppressWarnings("serial")
+@Slf4j
 public abstract class BasePage<T> extends GenericWebPage<T> {
+
+    private static final Duration SESSION_REFRESH_INTERVAL = Duration.minutes(5);
 
     public BasePage() {
         initNavbar();
@@ -35,5 +43,12 @@ public abstract class BasePage<T> extends GenericWebPage<T> {
         add(new BootstrapNavbarLink("config", ConfigPage.class, Model.of("Config"), "glyphicon glyphicon-wrench"));
         add(new BootstrapNavbarLink("aggregation", AggregationPage.class, Model.of("Aggregation"), "glyphicon glyphicon-search"));
         add(new BootstrapNavbarLink("admin", AdminPage.class, Model.of("Admin"), "glyphicon glyphicon-cog"));
+        add(new AbstractAjaxTimerBehavior(SESSION_REFRESH_INTERVAL) {
+
+            @Override
+            protected void onTimer(AjaxRequestTarget target) {
+                log.trace("Received AJAX request in order to keep http session alive.");
+            }
+        });
     }
 }
