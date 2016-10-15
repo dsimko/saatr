d10c1fec55ecaf1a358c5435e94708a57f317031
Daniel Šimko <dsimko@redhat.com>
2016-10-15 19:03:38 +0200

    Expand/collapse testcase panel

--- saatr/src/main/java/org/jboss/qa/tool/saatr/web/comp/build/testsuite/testcase/TestcasePanel.java
+++ saatr/src/main/java/org/jboss/qa/tool/saatr/web/comp/build/testsuite/testcase/TestcasePanel.java
@@ -2,8 +2,8 @@
 package org.jboss.qa.tool.saatr.web.comp.build.testsuite.testcase;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -12,6 +12,7 @@ import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.jboss.qa.tool.saatr.domain.build.TestcaseDocument;
 import org.jboss.qa.tool.saatr.domain.build.TestcaseDocument.Status;
 
@@ -56,36 +57,40 @@ public class TestcasePanel extends GenericPanel<TestcaseDocument> {
             body = new EmptyPanel("bodyPanel");
         }
         add(panel.setOutputMarkupId(true));
-        panel.add(new Label("name"));
         panel.add(body);
-        panel.add(new AjaxLink<Void>("collapse") {
+        WebMarkupContainer panelHead = new WebMarkupContainer("head");
+        panelHead.add(new AjaxEventBehavior("click") {
 
             @Override
-            public void onClick(AjaxRequestTarget target) {
-                replaceBodyPanel(new EmptyPanel(body.getId()));
+            protected void onEvent(AjaxRequestTarget target) {
+                if (body instanceof BodyPanel) {
+                    replaceBodyPanel(new EmptyPanel(body.getId()));
+                } else {
+                    replaceBodyPanel(new BodyPanel(body.getId(), model));
+                }
                 target.add(panel);
             }
+
+        });
+        panelHead.add(new Label("name"));
+        panelHead.add(new Label("collapse", Model.of("")) {
 
             @Override
             public boolean isVisible() {
                 return body instanceof BodyPanel;
             }
         });
-        panel.add(new AjaxLink<Void>("expand") {
-
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                replaceBodyPanel(new BodyPanel(body.getId(), model));
-                target.add(panel);
-            }
+        panelHead.add(new Label("expand", Model.of("")) {
 
             @Override
             public boolean isVisible() {
                 return body instanceof EmptyPanel;
             }
         });
+        panel.add(panelHead);
+
     }
-    
+
     private void replaceBodyPanel(Panel with) {
         body.replaceWith(with);
         body = with;
