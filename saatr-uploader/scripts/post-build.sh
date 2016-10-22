787de2994b5788dcb40b15d0f3206ea048ce9911
Daniel Šimko <dsimko@redhat.com>
2016-10-22 20:17:45 +0200

    upload script improvement

--- saatr-uploader/scripts/post-build.sh
+++ saatr-uploader/scripts/post-build.sh
@@ -2,7 +2,7 @@
 
 # https://wiki.jenkins-ci.org/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-JenkinsSetEnvironmentVariables
 function addAllVariables(){
-    declare -a arr=("NODE_NAME" "NODE_LABELS" "BUILD_URL" "TARGET" "DB" "JDK" "jdk" "EAP_VERSION" "LABEL" "OBJECT_STORE_TYPE")
+    declare -a arr=("NODE_NAME" "NODE_LABELS" "BUILD_URL" "TARGET" "DB" "JDK" "jdk" "EAP_VERSION" "LABEL" "OBJECT_STORE_TYPE", "AMQ_DISTRO", "IOTYPE")
     for i in "${arr[@]}"
     do
         var=$i
@@ -25,7 +25,6 @@ command="curl \
   -u saatr:S44TR! \
   -F \"jobName=${JOB_NAME-testing_job}\" \
   -F \"buildNumber=${BUILD_NUMBER-0}\" \
-  -F \"timestamp=$(date +%s)\" \
   $(addAllVariables)
   $(addReportsIfExists)
   http://46.183.65.66:14414/UploadServlet"
